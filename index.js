d509c417d2bd6313c88896467288d57c9d8457f6
Pulkit-s21 <pulkitsaxena795@gmail.com>
2022-03-08 10:46:48 +0530

    nothing major..either comments or formatting

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+// if we use data-attr in querySelector we need to put them in []
 const userCardTemplate = document.querySelector("[data-user-template]");
-const userCardContainer = document.querySelector("[data-user-cards-container");
+const userCardContainer = document.querySelector("[data-user-cards-container]");
 const searchInput = document.querySelector("[data-search]");
 
 searchInput.addEventListener("input", (e) => {
@@ -10,23 +11,23 @@ searchInput.addEventListener("input", (e) => {
         const isVisible = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value)
         // here we check if user is visible if yes then we pass in false skipping the class hide else we add in the class hide
         user.element.classList.toggle("hide", !isVisible);
-    })
+    });
 });
 
-let users = [] // created an empty array for users (users object)
+let users = []; // created an empty array for users (users object)
 
 // js promise
 fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json()).then(data => {
     
-    // here what we are saying is the card will return a docuemnt fragment and then copy the content of the userTemplate in the card and then clone everything inside it and then get the first child in that which is the card div
+    // here what we are saying is the card will return a document fragment and then copy the content of the userTemplate in the card and then clone everything inside it and then get the first child in that which is the card div
 
-    // user for loop cz we want to do this process for every user in the API
+    // used for loop cz we want to do this process for every user in the API
     // data.forEach(user => {
     //     const card = userCardTemplate.content.cloneNode(true).children[0];
 
-    //     const header = card.querySelector("[data-header]"); // if we use data-attr in querySelector we need to put them in [] 
+    //     const header = card.querySelector("[data-header]");
 
-    //     const body = card.querySelector("[data-body"); // if we use data-attr in querySelector we need to put them in []
+    //     const body = card.querySelector("[data-body]");
 
     //     header.innerText = user.name;
     //     body.innerText = user.email;
@@ -34,7 +35,7 @@ fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json()).then
     //     userCardContainer.appendChild(card);
     // });
 
-    // settings user to whtever we return here and to return we need to use map so thats why we chngd from forEach to map
+    // setting user to whtever we return here and to return we need to use map so thats why we chngd from forEach to map
     
     users = data.map(user => {
         const card = userCardTemplate.content.cloneNode(true).children[0];
@@ -48,6 +49,6 @@ fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json()).then
 
         userCardContainer.appendChild(card);
 
-        return {name: user.name, email:user.email, element:card}
+        return {name: user.name, email: user.email, element: card}
     });
 });
